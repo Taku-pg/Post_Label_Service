1cf1bcb4536ce8b2f529c94ac9d8ace156f77799
Taku-pg <kaitsu505@gmail.com>
2026-05-07 12:55:11 +0200

    New: create delivery type page

---
+++ Frontend/src/app/delivery-type/delivery-type.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-type',
+  imports: [],
+  templateUrl: './delivery-type.html',
+  styleUrl: './delivery-type.css',
+})
+export class DeliveryType {
+
+}
