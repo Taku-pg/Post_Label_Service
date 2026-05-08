d10ab82058eae20f9cca44477ca3b5d1ef16c0f1
Taku-pg <kaitsu505@gmail.com>
2026-05-08 15:09:07 +0200

    New: create delivery info page

---
+++ Frontend/src/app/views/delivery-information/delivery-information.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-information',
+  imports: [],
+  templateUrl: './delivery-information.html',
+  styleUrl: './delivery-information.css',
+})
+export class DeliveryInformation {
+
+}
