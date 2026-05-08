e11bbb922d59ba3b786af046c138cdf428b69d52
Taku-pg <kaitsu505@gmail.com>
2026-05-08 22:29:21 +0200

    New: cretate content info page

---
+++ Frontend/src/app/views/content-information/content-information.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+
+@Component({
+  selector: 'app-content-information',
+  imports: [],
+  templateUrl: './content-information.html',
+  styleUrl: './content-information.css',
+})
+export class ContentInformation {
+
+}
