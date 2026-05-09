5691e65e0a7ad1e31a8e597b0e91c2909415a308
Taku-pg <kaitsu505@gmail.com>
2026-05-09 14:53:59 +0200

    Update: create shared item table component

---
+++ Frontend/src/app/view-components/item-table/item-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-table',
+  imports: [],
+  templateUrl: './item-table.html',
+  styleUrl: './item-table.css',
+})
+export class ItemTable {
+
+}
--- Frontend/src/app/views/content-information/content-information.ts
+++ Frontend/src/app/views/content-information/content-information.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Progress } from "../../view-components/progress/progress";
+import { ItemTable } from "../../view-components/item-table/item-table";
 
 @Component({
   selector: 'app-content-information',
-  imports: [Progress],
+  imports: [Progress, ItemTable],
   templateUrl: './content-information.html',
   styleUrl: './content-information.css',
 })
