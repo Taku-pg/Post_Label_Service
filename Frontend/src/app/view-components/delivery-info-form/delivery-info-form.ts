b4abe154e4217cd3e77cdb94a42ce1dff8d3540e
Taku-pg <kaitsu505@gmail.com>
2026-05-09 15:01:38 +0200

    Update: create shared delivery info component

---
+++ Frontend/src/app/view-components/delivery-info-form/delivery-info-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-info-form',
+  imports: [],
+  templateUrl: './delivery-info-form.html',
+  styleUrl: './delivery-info-form.css',
+})
+export class DeliveryInfoForm {
+
+}
--- Frontend/src/app/views/delivery-information/delivery-information.ts
+++ Frontend/src/app/views/delivery-information/delivery-information.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Progress } from "../../view-components/progress/progress";
+import { DeliveryInfoForm } from "../../view-components/delivery-info-form/delivery-info-form";
 
 @Component({
   selector: 'app-delivery-information',
-  imports: [Progress],
+  imports: [Progress, DeliveryInfoForm],
   templateUrl: './delivery-information.html',
   styleUrl: './delivery-information.css',
 })
