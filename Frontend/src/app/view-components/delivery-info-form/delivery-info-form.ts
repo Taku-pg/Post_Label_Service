3030d03c79e07b0d393dac595d3dbc9dd4b9b668
Taku-pg <kaitsu505@gmail.com>
2026-05-09 17:31:56 +0200

    Update: add readonly property

--- Frontend/src/app/view-components/delivery-info-form/delivery-info-form.ts
+++ Frontend/src/app/view-components/delivery-info-form/delivery-info-form.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-delivery-info-form',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './delivery-info-form.css',
 })
 export class DeliveryInfoForm {
-
+  isReadOnly = input<boolean>(false);
 }
