52dc5fa1368da13cc970780ae0110a206e74df0b
Taku-pg <kaitsu505@gmail.com>
2026-05-22 00:13:40 +0200

    Update: add widget

--- Frontend/src/app/views/delivery-search/delivery-search.ts
+++ Frontend/src/app/views/delivery-search/delivery-search.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { DeliveryService } from '../../services/delivery.service';
 import { Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-delivery-search',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './delivery-search.html',
   styleUrl: './delivery-search.css',
 })
@@ -12,11 +13,20 @@ export class DeliverySearch implements OnInit{
   private _router = inject(Router);
   private _deliveryService = inject(DeliveryService);
 
+  trackingIdControl = new FormControl('', Validators.required);
+
   ngOnInit(){
     this._deliveryService.reset();
   }
 
   onClickSearch(){
+    if(this.trackingIdControl.invalid){
+      this.trackingIdControl.markAsTouched();
+      return;
+    }
+  }
 
+  onClickBack(){
+    this._router.navigate(['delivery-type']);
   }
 }
