224108aed4c93ecec29d29febc4fa3fc55bf8b26
Taku-pg <kaitsu505@gmail.com>
2026-05-21 23:20:41 +0200

    Update: add methods

--- Frontend/src/app/views/content-information/content-information.ts
+++ Frontend/src/app/views/content-information/content-information.ts
@@ -1,9 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
 import { Progress } from "../../view-components/progress/progress";
 import { ItemTable } from "../../view-components/item-table/item-table";
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AddItemForm } from '../../view-components/add-item-form/add-item-form';
 import { Item } from '../../models/item.model';
+import { Router } from '@angular/router';
+import { DeliveryService } from '../../services/delivery.service';
+import { ConfirmDialog } from '../../view-components/confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-content-information',
@@ -13,6 +16,8 @@ import { Item } from '../../models/item.model';
 })
 export class ContentInformation {
   private _matDialog = inject(MatDialog); 
+  private _router = inject(Router);
+  private _deliveryService = inject(DeliveryService);
 
   receiveNewItem(item: Item){
     console.log('received');
@@ -25,5 +30,31 @@ export class ContentInformation {
     dialogConfig.width = '80%';
 
     const modalDialog = this._matDialog.open(AddItemForm, dialogConfig);
+  }
+
+  onClickBack(){
+    this._router.navigate(['delivery-info']);
+  }
+
+  onClickNext(){
+    const contents = this._deliveryService.deliveryInfo().contents;
+    if(contents.length === 0){
+      this.callDialog();
+      return;
+    }
+    this._router.navigate(['confirmation']);
+  }
+
+  callDialog(){
+    const dialogConfig = new MatDialogConfig;
+        dialogConfig.height = '35%';
+        dialogConfig.width = '40%';
+        dialogConfig.data = {
+          title: 'No Item',
+          message: 'Please input at least 1 item',
+          type: 'ok'
+        }
+    
+        const modalDialog = this._matDialog.open(ConfirmDialog, dialogConfig)
   }
 }
