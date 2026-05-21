f7ebfd6d74cfd9648ab33b384fd556cf2f1401aa
Taku-pg <kaitsu505@gmail.com>
2026-05-21 23:20:27 +0200

    Update: update click next method

--- Frontend/src/app/views/delivery-information/delivery-information.ts
+++ Frontend/src/app/views/delivery-information/delivery-information.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
 import { Progress } from "../../view-components/progress/progress";
 import { DeliveryInfoForm } from "../../view-components/delivery-info-form/delivery-info-form";
 import { Router } from '@angular/router';
@@ -6,6 +6,7 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ConfirmDialog } from '../../view-components/confirm-dialog/confirm-dialog';
 import { DeliveryInfo } from '../../models/deliveryInfo.model';
 import { DeliveryService } from '../../services/delivery.service';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-delivery-information',
@@ -20,48 +21,86 @@ export class DeliveryInformation {
   formElement = viewChild<DeliveryInfoForm>('formElement');
 
 
-  onClickNext(){
+  async onClickNext() {
     const isValid = this.formElement()?.validate();
 
-    if(isValid){
+    if (isValid) {
       const formData = this.formElement()?.formValue()!;
-      const deliveryInfo: DeliveryInfo = {
-        sender: {
-          firstName: formData.firstName!,
-          lastName: formData.lastName!,
-          email: formData.email!,
-          phone: formData.phone!,
-          company: formData.company?? null
-        },
-        senderAddress: {
-          street: formData.senderStreet!,
-          city: formData.senderCity!,
-          zip: formData.senderZip!,
-          country: formData.senderCountry?? null,
-        },
-        receiverAddress: {
-          street: formData.receiverStreet!,
-          city: formData.receiverCity!,
-          zip: formData.receiverZip!,
-          country: formData.receiverCountry?? null,
-        },
-        deliveryOption: formData.deliveryOption!,
-        deliveryPurpose: formData.deliveryPurpose?? null
+      const deliveryPurpose = formData.deliveryPurpose;
+
+      if(deliveryPurpose === "sales"){
+        const confirm = await this.callDialog(
+          'Caution',
+          'This delivery will be imposed addtional tax.\nDo you agree?',
+          'confirm'
+        );
+
+        console.log(confirm);
+
+        if(!confirm!){
+          return;
+        }
+        
       }
+
+      const deliveryInfo: DeliveryInfo = this.setValue(formData);
+
       this._deliveryService.setDeliveryInfo(deliveryInfo);
       this._router.navigate(['contents']);
     }
   }
 
-  onClickBack(){
+  async onClickBack() {
+    const res = await this.callDialog(
+      'Caution',
+      'Input information will be discarded.\nAre you sure ?',
+      'confirm');
+
+      if (res!) {
+        this._router.navigate(['delivery-type']);
+      }
+  }
+
+
+  setValue(formData: any): DeliveryInfo {
+    const deliveryInfo: DeliveryInfo = {
+      sender: {
+        firstName: formData.firstName!,
+        lastName: formData.lastName!,
+        email: formData.email!,
+        phone: formData.phone!,
+        company: formData.company ?? null
+      },
+      senderAddress: {
+        street: formData.senderStreet!,
+        city: formData.senderCity!,
+        zip: formData.senderZip!,
+        country: formData.senderCountry ?? null,
+      },
+      receiverAddress: {
+        street: formData.receiverStreet!,
+        city: formData.receiverCity!,
+        zip: formData.receiverZip!,
+        country: formData.receiverCountry ?? null,
+      },
+      deliveryOption: formData.deliveryOption!,
+      deliveryPurpose: formData.deliveryPurpose ?? null
+    }
+    return deliveryInfo;
+  }
+
+  async callDialog(title: String, message: string, type: string){
     const dialogConfig = new MatDialogConfig;
     dialogConfig.height = '35%';
     dialogConfig.width = '40%';
     dialogConfig.data = {
-      title: 'Caution',
-      message: 'Input information will be discarded.\nAre you OK ?'
+      title: title,
+      message: message,
+      type: type
     }
 
-    const modalDialog = this._matDialog.open(ConfirmDialog, dialogConfig)
+    const modalDialog = this._matDialog.open(ConfirmDialog, dialogConfig);
+    const res = await firstValueFrom(modalDialog.afterClosed());
+    return res;
   }
 }
