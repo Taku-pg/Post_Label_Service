cb8aa65ae1554634d57448a2848356f4b9c65ad4
Taku-pg <kaitsu505@gmail.com>
2026-05-23 15:37:36 +0200

    Update: update style

--- Frontend/src/app/views/delivery-search/delivery-search.ts
+++ Frontend/src/app/views/delivery-search/delivery-search.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { DeliveryService } from '../../services/delivery.service';
 import { Router } from '@angular/router';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-delivery-search',
@@ -12,6 +13,7 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 export class DeliverySearch implements OnInit{
   private _router = inject(Router);
   private _deliveryService = inject(DeliveryService);
+  private _apiService = inject(ApiService);
 
   trackingIdControl = new FormControl('', Validators.required);
 
@@ -20,13 +22,32 @@ export class DeliverySearch implements OnInit{
   }
 
   onClickSearch(){
+    this.setNotFoundError();
+    return;
     if(this.trackingIdControl.invalid){
       this.trackingIdControl.markAsTouched();
       return;
     }
+    const trackingId = this.trackingIdControl.value;
+    this._apiService.getDeliveryByTrackingId(trackingId!).subscribe({
+      next: (res)=>{
+
+      },
+      error: ()=>{
+        this.setNotFoundError();
+      }
+    });
   }
 
   onClickBack(){
     this._router.navigate(['delivery-type']);
+  }
+
+  setNotFoundError(){
+    console.log('called');
+    this.trackingIdControl.setErrors({
+      notFound: true
+    });
+    this.trackingIdControl.markAsTouched();
   }
 }
