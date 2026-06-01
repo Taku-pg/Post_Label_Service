9ddac1df649e0cc4877af80cd2e50a45ea3ed3a6
Taku-pg <kaitsu505@gmail.com>
2026-06-01 22:15:14 +0200

    Update: add condition

--- Frontend/src/app/views/delivery-detail/delivery-detail.ts
+++ Frontend/src/app/views/delivery-detail/delivery-detail.ts
@@ -1,8 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { DeliveryInfoForm } from "../../view-components/delivery-info-form/delivery-info-form";
 import { ItemTable } from "../../view-components/item-table/item-table";
 import { Router } from '@angular/router';
-import { DeliveryService } from '../../services/delivery.service';
 import { DeliveryStatusService } from '../../services/delivery_status.service';
 
 @Component({
@@ -15,6 +14,10 @@ export class DeliveryDetail {
   private _router = inject(Router);
   private _deliveryStatusService = inject(DeliveryStatusService);
   deliveryStatus = this._deliveryStatusService.deliveryStatus;
+
+  isInternaional = computed(()=>{
+    return this.deliveryStatus().delivery?.deliveryType === 'international';
+  })
 
   onClickBack(){
     this._router.navigate(['delivery-overview']);
